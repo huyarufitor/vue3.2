9ce333eb7dcf9ce3eb1050de0f79a08a9a57c2b3
fitorhu <fitorhu@zego.im>
2022-02-13 22:55:49 +0800

    登录页面-静态布局

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -2,5 +2,9 @@
 // We suggest you to commit this file into source control
 declare global {
   const ElButton: typeof import('element-plus/es')['ElButton']
+  const ElForm: typeof import('element-plus/es')['ElForm']
+  const ElFormItem: typeof import('element-plus/es')['ElFormItem']
+  const ElIcon: typeof import('element-plus/es')['ElIcon']
+  const ElInput: typeof import('element-plus/es')['ElInput']
 }
 export {}
