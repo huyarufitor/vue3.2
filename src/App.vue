33f30435f459fa1b311fbea04149fe632f560f54
fitorhu <fitorhu@zego.im>
2022-02-13 22:06:46 +0800

    ✨ vue3新特性：template和css用变量写

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,23 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <el-input v-model="input" placeholder="Please input" />
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
+  <div class="review-wrap"></div>
   <router-view />
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
+// import { ref } from "vue";
 
-const input = ref("2212"); // 默认绑定的值
+// const input = ref("2212"); // 默认绑定的值
+const boxwidth = "100px";
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.review-wrap {
+  width: v-bind(boxwidth);
+  height: 100px;
+  background: #eee;
 }
 </style>
